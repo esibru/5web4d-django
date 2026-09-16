01b36afeb3331980c1fc4129806188ae03005475
Sébastien Drobisz <sdrobisz@he2b.be>
2026-09-16 09:51:46 +0200

    audit fix + liste prof

---
+++ src/data/teachers.js
@@ -0,0 +1,30 @@
+export const teachers = [
+  {
+    trigram: "NRI",
+    firstName: "Nicolas",
+    lastName: "Richard",
+    webPage: "http://ects.esi-bru.be/online/enseignants/NRI.html",
+    email: "nrichard@he2b.be",
+  },
+  {
+    trigram: "PHA",
+    firstName: "Pierre",
+    lastName: "Hauweele",
+    webPage: "http://ects.esi-bru.be/online/enseignants/pha.html",
+    email: "phauweele@he2b.be",
+  },
+  {
+    trigram: "SDR",
+    firstName: "Sébastien",
+    lastName: "Drobisz",
+    webPage: "http://ects.esi-bru.be/online/enseignants/sdr.html",
+    email: "sdrobisz@he2b.be",
+  },
+  {
+    trigram: "TNI",
+    firstName: "Thibaut",
+    lastName: "Nicodème",
+    webPage: "http://ects.esi-bru.be/online/enseignants/tni.html",
+    email: "tnicodeme@he2b.be",
+  },
+];
